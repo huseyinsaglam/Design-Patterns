55eda9c22fcc446c2ec5f8d11111720f5e129325
Huseyin Saglam <hsaglam001@gmail.com>
2020-03-22 17:13:38 +0300

    Null-Object-Design

---
+++ NullObjectDesignPattern/src/com/NullObject/AbstractCustomer.java
@@ -0,0 +1,9 @@
+package com.NullObject;
+
+public abstract class AbstractCustomer {
+
+	
+	   protected String name;
+	   public abstract boolean isNil(); // sýfýrMý, bosMu
+	   public abstract String getName();
+}
---
+++ NullObjectDesignPattern/src/com/NullObject/NullCustomer.java
@@ -0,0 +1,15 @@
+package com.NullObject;
+
+public class NullCustomer extends AbstractCustomer{
+
+	   @Override
+	   public String getName() {
+	      return "Not Available in Customer Database";
+	   }
+
+	   @Override
+	   public boolean isNil() {
+	      return true;
+	   }
+
+}
